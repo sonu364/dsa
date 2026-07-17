eb0bb1abf9879076ce7c49b7f94b5afd09c425a7
Rohit Santosh Sawase <rohitsawase5@gmail.com>
2026-07-17 13:55:02 +0530

    Time: 16 ms (14.60%) | Memory: 21.3 MB (10.25%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -1,8 +1,21 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-      sorted_list = []
-      for num in nums:
-        sorted_list.append(num ** 2)
-      sorted_list.sort()
-      return sorted_list
+        left = 0
+        right = len(nums) - 1
+        result = []
         
+        # Loop until the two pointers meet
+        while left <= right:
+            left_square = nums[left] ** 2
+            right_square = nums[right] ** 2
+            
+            # Compare and add the LARGER square to the back of our result list
+            if left_square > right_square:
+                result.append(left_square)
+                left += 1
+            else:
+                result.append(right_square)
+                right -= 1
+                
+        # Since we collected them from largest to smallest, reverse it at the end!
+        return result[::-1]
