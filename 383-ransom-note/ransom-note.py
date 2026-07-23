d432e12abced6030790774dd02c8f4198688b8d3
Rohit Santosh Sawase <rohitsawase5@gmail.com>
2026-07-23 17:38:30 +0530

    Time: 3 ms (96.93%) | Memory: 19.6 MB (71.60%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,5 +1,8 @@
-from collections import Counter
-
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        return Counter(ransomNote) <= Counter(magazine)
+        for ch in ransomNote:
+            if ch not in magazine:
+                return False
+            magazine = magazine.replace(ch,"",1)
+        return True
+        
