0b09317792686aed07c022d955f1d255c41e5e31
Rohit Santosh Sawase <rohitsawase5@gmail.com>
2026-07-28 17:42:14 +0530

    Modify containsDuplicate to return duplicate numbers
    Refactor containsDuplicate method to return duplicates.

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -8,3 +8,18 @@ class Solution:
             seen.add(num)
 
         return False
+
+from typing import List
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]):
+        seen = set()
+        duplicates = set()
+
+        for num in nums:
+            if num in seen:
+                duplicates.add(num)
+            else:
+                seen.add(num)
+
+        return list(duplicates)
